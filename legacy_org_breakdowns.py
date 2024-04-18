041bd809c85a538567cae2b2697ea07c7f762f1f
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-04-18 15:14:54 +0000

    moved group and count code into function

--- legacy_org_breakdowns.py
+++ legacy_org_breakdowns.py
@@ -5,27 +5,57 @@ from src.data_imports import data_imports
 
 df_survey = data_imports.import_survey_to_df()
 
-for col in df_survey.columns:
-    print("'" + col + "'")
+# for col in df_survey.columns:
+#     print("'" + col + "'")
 
 group_column = 'What is your legacy team?\xa0\xa0'
 count_column = 'Which platforms have you used in the last 12 months? '
 
-df_survey_with_group_and_count_columns_only = df_survey[[group_column, count_column]]
+multiple_choice_cols = [
+    'Which platforms have you used in the last 12 months? ',
+    'Which cloud computing services do you have access to?',
+    'Which programming languages have you used in the last 12 months? ',
+    'Which organisational cloud repositories do you have access to?',
+    'Which dashboarding tools do you have access to?',
+    'Where do you store your project files?',
+    'What do you use as team knowledge store? ',
+    'Which tools do you use for project management?',
+    'What do you typically use for project team communication',
+    'What tools do you use for stakeholder engagement?',
+    'Which of the following do you use for work? (select multiple if it applies)',
+]
 
-print(df_survey_with_group_and_count_columns_only)
+def split_and_explode_column(df : pd.DataFrame, col : str, sep : str = ";"):
 
-df_survey_with_group_and_count_columns_only[count_column] = (
-    df_survey_with_group_and_count_columns_only[count_column].str.split(";")
+    df[col] = (
+        df[col].str.split(sep)
+    )
+
+    df = df.explode(col)
+
+    return df
+
+def group_and_count_cols(df : pd.DataFrame, col_list : list):
+
+    count_col_name = "counts"
+
+    df = df[col_list].copy()
+
+    for col in col_list:
+        if col in multiple_choice_cols:
+            df[col] = (
+                df[col].str.split(";")
+            )
+
+            df = df.explode(col)
+            
+    df = df.groupby(col_list).size().reset_index(name=count_col_name)
+    print(col_list[:-1] + [count_col_name]
+    )
+    df = df.sort_values(col_list[:-1] + [count_col_name], ascending = False)
+
+    return df
+
+print(
+    group_and_count_cols(df_survey, [group_column, count_column])
 )
-
-df_survey_count_column_exploded = (df_survey_with_group_and_count_columns_only
-    .explode(count_column)
-)
-
-df_platform_by_legacy_org = df_survey_count_column_exploded.groupby([group_column, count_column]).size().reset_index(name='counts')
-
-df_platform_by_legacy_org = df_platform_by_legacy_org.sort_values([group_column, "counts"], ascending = False)
-
-print(df_platform_by_legacy_org)
-
