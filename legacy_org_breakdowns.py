83550223d3f1c01f64f647ef5d08ba495b3cf606
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-04-18 16:02:40 +0000

    clean up

--- legacy_org_breakdowns.py
+++ legacy_org_breakdowns.py
@@ -6,16 +6,16 @@ from src.processing import aggregations
 
 df_survey = data_imports.import_survey_to_df()
 
-# for col in df_survey.columns:
-#     print("'" + col + "'")
-
 group_column = 'What is your legacy team?\xa0\xa0'
-count_column = 'Which platforms have you used in the last 12 months? '
-
-
-
-
 
 print(
-    aggregations.group_and_count_cols(df_survey, [group_column, count_column])
+    aggregations.group_and_count_cols(df_survey, [group_column, 'Which platforms have you used in the last 12 months? '])
+)
+
+print(
+    aggregations.group_and_count_cols(df_survey, [group_column, 'I can do my job with the tools available to me '])
+)
+
+print(
+    aggregations.group_and_count_cols(df_survey, [group_column, 'Which of the following do you use for work? (select multiple if it applies)'])
 )
